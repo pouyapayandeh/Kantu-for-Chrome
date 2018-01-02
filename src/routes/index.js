78feefd813d8b5e5a7234c511e3661eaec805f9e
mortezaho.1376@gmail.com <mortezaho.1376@gmail.com>
2018-01-02 17:55:03 +0330

    trying open a pafe

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import DashboardPage from '../containers/dashboard'
 import AuthPage from '../containers/auth'
 import MainPage from '../containers/main'
+import PartitionPage from '../containers/partition'
 
 const routes = [
   { path: '/dashboard', component: DashboardPage, exact: true },
   { path: '/auth', component: AuthPage, exact: true },
-  { path: '/main', component: MainPage, exact: true }
+  { path: '/main', component: MainPage, exact: true },
+  { path: '/partition', component: PartitionPage, exact: true }
 ];
 
 export default routes;
