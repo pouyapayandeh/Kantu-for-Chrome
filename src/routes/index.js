02db5b6ad620e0b2e3ac5908de0b1a86dcf8b7d0
pouyapayandeh <pouya.payandeh@gmail.com>
2018-01-02 15:20:41 +0330

    Result Page Added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import DashboardPage from '../containers/dashboard'
 import AuthPage from '../containers/auth'
 import MainPage from '../containers/main'
+import ResultPage from '../containers/result'
 
 const routes = [
   { path: '/dashboard', component: DashboardPage, exact: true },
+  { path: '/result', component: ResultPage, exact: true },
   { path: '/auth', component: AuthPage, exact: true },
   { path: '/main', component: MainPage, exact: true }
 ];
