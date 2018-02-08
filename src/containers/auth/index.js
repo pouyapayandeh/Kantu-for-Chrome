1960feff67070e1cf227f04a918560977a6af948
mortezaho.1376@gmail.com <mortezaho.1376@gmail.com>
2018-02-08 18:10:23 +0330

    right structure

--- src/containers/auth/index.js
+++ src/containers/auth/index.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators }  from 'redux'
-import { Link } from 'react-router-dom';
+import { Link, Route, Switch, Redirect, withRouter } from 'react-router-dom'
+import routes from '../../routes'
 
 
 import * as actions from '../../actions'
 
 class Auth extends React.Component {
+
 	onClickLogin = () => {
-		this.props.history.push('/dashboard');
-	  }
+		this.props.loginCallback()
+	}
+
 	render () {
 		return (
+			
 			<div className="auth">
 				Username: <input type="text" className="auth-input" id="username"/>
 				<br />
@@ -31,3 +35,4 @@ export default connect(
 	state => ({}),
 	dispatch => bindActionCreators({...actions}, dispatch)
 )(Auth)
+
--- src/containers/main/index.js
+++ src/containers/main/index.js
@@ -1,25 +1,75 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators }  from 'redux'
 import { HashHistory as Router, Route, Link, Switch, Redirect } from 'react-router-dom'
+import { Button } from 'antd'
 
-import routes from '../../routes'
+import * as actions from '../../actions'
+import csIpc from '../../common/ipc/ipc_cs'
 import Header from '../../components/header'
-import Sidebar from '../../components/sidebar'
-import Dashboard from '../dashboard'
+import Sidebar from '../../containers/sidebar'
+import DashboardPage from '../../containers/dashboard'
 import 'antd/dist/antd.css'
 import '../../app.scss'
 
 class Main extends Component {
+  hideBackupAlert = () => {
+    this.props.updateConfig({
+      lastBackupActionTime: new Date() * 1
+    })
+    this.$app.classList.remove('with-alert')
+  }
+
+  onClickBackup = () => {
+    this.props.runBackup()
+    this.hideBackupAlert()
+  }
+
+  onClickNoBackup = () => {
+    this.hideBackupAlert()
+  }
+
+  componentDidMount () {
+    const run = () => {
+      csIpc.ask('PANEL_TIME_FOR_BACKUP', {})
+      .then(isTime => {
+        if (!isTime)  return
+        this.$app.classList.add('with-alert')
+      })
+    }
+
+    // Note: check whether it's time for backup every 5 minutes
+    this.timer = setInterval(run, 5 * 60000)
+    run()
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.timer)
+  }
+
   render () {
     return (
-      <div className="main">
-        <Sidebar />
-        <section className="content">
-          <Header />
-          <Dashboard />
-        </section>
+      <div className="app with-sidebar" ref={el => { this.$app = el }}>
+        <div className="backup-alert">
+          <span>Do you want to run the automated backup?</span>
+          <span className="backup-actions">
+            <Button type="primary" onClick={this.onClickBackup}>Yes</Button>
+            <Button onClick={this.onClickNoBackup}>No</Button>
+          </span>
+        </div>
+        <div className="app-inner">
+          <Sidebar />
+          <section className="content">
+            <Header />
+            <DashboardPage />
+          </section>
+        </div>
       </div>
     );
   }
 }
 
-export default Main;
+export default connect(
+  state => ({}),
+  dispatch => bindActionCreators({...actions}, dispatch)
+)(Main)
